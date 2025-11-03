34031e951f74b78be617bbd0c95c036ef4aa008c
Martin Hosken <martin_hosken@sil.org>
2025-11-03 15:39:04 +0700

    Lower coeng positional fixes

--- tools/fix_busra_aps.py
+++ tools/fix_busra_aps.py
@@ -37,7 +37,7 @@ def some(f):
             mid = 0.5 * (b.xmin + b.xmax)
             f(g, anchors, b, g.width)
             g.anchors = [v for k, v in sorted(anchors.items())]
-            cover.add_glyph(n, 2)
+            # cover.add_glyph(n, 2)
     return fun
 
 def unilist(a, suffices=[], prefix="uni17"):
@@ -141,127 +141,144 @@ centre_shift = 49
 u_base = -715 + 1580j
 shifts = {'uni17B1': 60j, 'uni17B3': 100j, 'uni17AA': 260j}
 
-cover = Coverage()
-for g in allglyphs:
-    cover.add_glyph(g, 1)
+#cover = Coverage()
+#for g in allglyphs:
+#    cover.add_glyph(g, 1)
 
-@some
-def bases(g, a, b, adv):
-    name = g.name
-    old = a.copy()
-    a.clear()
-    if 'N' in old:
-        c = anchorVal(old['N']) + l_shift
-    else:
-        c = (b.xmin + b.xmax) // 2
-    if b.ymin < -100:
-        c += 1j * b.ymin
-    addAnchorVal(a, 'LC', c)
-    r = adv + l_shift
-    addAnchorVal(a, 'L', r + (1j * b.ymin if b.ymin < -100 else 0))
-    addAnchorVal(a, 'U', r + 1580j + baseheights.get(name, 0))
-    if 'H' in old:
-        u = anchorVal(old['H']) - 700   # all udias have _H = -700
-    else:
-        u = adv + u_base
-    u += shifts.get(name, 0)
-    if name in centre_left:
-        u += centre_shift
-    addAnchorVal(a, 'UC', u + baseheights.get(name, 0))
-
-def getbasepos(g, aname, b, ratio, shift):
-    if g is not None:
-        for a in g.anchors:
-            if a.name == aname:
-                return anchorVal(a)
-    return (b.xmin + b.xmax) // ratio
-
-@some
-def clear_aps(g, a, b, adv):
-    if g.name.startswith("uni17") and not cover.has_glyph(g.name, 2):
+if 0:
+    @some
+    def bases(g, a, b, adv):
+        name = g.name
+        old = a.copy()
         a.clear()
+        if 'N' in old:
+            c = anchorVal(old['N']) + l_shift
+        else:
+            c = (b.xmin + b.xmax) // 2
+        if b.ymin < -100:
+            c += 1j * b.ymin
+        addAnchorVal(a, 'LC', c)
+        r = adv + l_shift
+        addAnchorVal(a, 'L', r + (1j * b.ymin if b.ymin < -100 else 0))
+        addAnchorVal(a, 'U', r + 1580j + baseheights.get(name, 0))
+        if 'H' in old:
+            u = anchorVal(old['H']) - 700   # all udias have _H = -700
+        else:
+            u = adv + u_base
+        u += shifts.get(name, 0)
+        if name in centre_left:
+            u += centre_shift
+        addAnchorVal(a, 'UC', u + baseheights.get(name, 0))
+
+    def getbasepos(g, aname, b, ratio, shift):
+        if g is not None:
+            for a in g.anchors:
+                if a.name == aname:
+                    return anchorVal(a)
+        return (b.xmin + b.xmax) // ratio
+
+    @some
+    def clear_aps(g, a, b, adv):
+        if g.name.startswith("uni17") and not cover.has_glyph(g.name, 2):
+            a.clear()
+
+    @some
+    def ligs(g, a, b, adv):
+        name = g.name
+        old = a.copy()
+        a.clear()
+        base = name[:7] + (name[name.index("."):] if "." in name else "")
+        base = base.replace(".right", "")
+        if base in font:
+            baseg = font[base]
+        else:
+            baseg = None
+        c = getbasepos(baseg, "LC", b, 3, 0)
+        addAnchorVal(a, 'LC_1', c)
+        c = b.xmax - 700 # (half stem width, what a hack)
+        addAnchorVal(a, "LC_2", c)
+        c = getbasepos(baseg, "L", b, 2, 0)
+        addAnchorVal(a, "L_1", c)
+        addAnchorVal(a, "U_1", c + 1580j + baseheights.get(base, 0))
+        addAnchorVal(a, "L_2", adv + l_shift)
+        addAnchorVal(a, "U_2", adv + l_shift + 1580j + baseheights.get(base, 0))
+        c = getbasepos(baseg, "UC", b, 3, 1580j)
+        addAnchorVal(a, "UC_1", c + baseheights.get(base, 0))
+        c = b.xmax - 700
+        addAnchorVal(a, "UC_2", c + 1580j + baseheights.get(base, 0))
+
+    @some
+    def aboveright(g, a, b, adv):
+        old = a.copy()
+        a.clear()
+        addAnchorVal(a, "_U", l_shift + 1580j)
+        addAnchorVal(a, "_UU", l_shift + 20 + 1580j)
+        addAnchorVal(a, "UU", l_shift + 20 + 1j * max(b.ymax + 40, l_shift.imag + 1860))
+        addAnchorVal(a, "UA", u_base.real + 20 + 1j * max(b.ymax + 40, u_base.imag + 280))
+
+    @some
+    def abovecentre(g, a, b, adv):
+        old = a.copy()
+        a.clear()
+        addAnchorVal(a, "_UC", u_base)
+        addAnchorVal(a, "_UA", u_base + 20)
+        above = u_base + 20 + 280j
+        addAnchorVal(a, "UA", above.real + 1j * max(b.ymax + 40, above.imag))
+        addAnchorVal(a, "UU", l_shift + 20 + max(above.imag, 1860, b.ymax + 40) * 1j)
+
+    @some
+    def belowright(g, a, b, adv):
+        old = a.copy()
+        a.clear()
+        offset = -670j if "low" in g.name else 0
+        addAnchorVal(a, "_L", l_shift + offset)
+        addAnchorVal(a, "_LL", l_shift + 20 + offset)
+        addAnchorVal(a, "LL", l_shift + 20 - 670j + offset)
+        if "def" in old:
+            c = anchorVal(old["def"])
+        else:
+            c = (b.xmin + b.xmax) // 2
+        addAnchorVal(a, "LA", c - 20 - 670j)
+
+    @some
+    def belowcentre(g, a, b, adv):
+        old = a.copy()
+        a.clear()
+        if "def" in old:
+            c = anchorVal(old["def"])
+        else:
+            c = (b.xmin + b.xmax) // 2
+        addAnchorVal(a, "_LC", c)
+        addAnchorVal(a, "_LA", c - 20)
+        addAnchorVal(a, "LA", c - 20 - 670j)
+        addAnchorVal(a, "LL", l_shift + 20 - 670j)
 
 @some
-def ligs(g, a, b, adv):
-    name = g.name
+def belowlow(g, a, b, adv):
+    if "low" not in g.name:
+        return
+    if not g.name.startswith("uni17D2"):
+        return
     old = a.copy()
     a.clear()
-    base = name[:7] + (name[name.index("."):] if "." in name else "")
-    base = base.replace(".right", "")
-    if base in font:
-        baseg = font[base]
-    else:
-        baseg = None
-    c = getbasepos(baseg, "LC", b, 3, 0)
-    addAnchorVal(a, 'LC_1', c)
-    c = b.xmax - 700 # (half stem width, what a hack)
-    addAnchorVal(a, "LC_2", c)
-    c = getbasepos(baseg, "L", b, 2, 0)
-    addAnchorVal(a, "L_1", c)
-    addAnchorVal(a, "U_1", c + 1580j + baseheights.get(base, 0))
-    addAnchorVal(a, "L_2", adv + l_shift)
-    addAnchorVal(a, "U_2", adv + l_shift + 1580j + baseheights.get(base, 0))
-    c = getbasepos(baseg, "UC", b, 3, 1580j)
-    addAnchorVal(a, "UC_1", c + baseheights.get(base, 0))
-    c = b.xmax - 700
-    addAnchorVal(a, "UC_2", c + 1580j + baseheights.get(base, 0))
+    for k, v in old.items():
+        if k[0:2] == "_L":
+            v['y'] = 0
+        addAnchor(a, g, k, v['x'], v['y'])
 
-@some
-def aboveright(g, a, b, adv):
-    old = a.copy()
-    a.clear()
-    addAnchorVal(a, "_U", l_shift + 1580j)
-    addAnchorVal(a, "_UU", l_shift + 20 + 1580j)
-    addAnchorVal(a, "UU", l_shift + 20 + 1j * max(b.ymax + 40, l_shift.imag + 1860))
-    addAnchorVal(a, "UA", u_base.real + 20 + 1j * max(b.ymax + 40, u_base.imag + 280))
 
-@some
-def abovecentre(g, a, b, adv):
-    old = a.copy()
-    a.clear()
-    addAnchorVal(a, "_UC", u_base)
-    addAnchorVal(a, "_UA", u_base + 20)
-    above = u_base + 20 + 280j
-    addAnchorVal(a, "UA", above.real + 1j * max(b.ymax + 40, above.imag))
-    addAnchorVal(a, "UU", l_shift + 20 + max(above.imag, 1860, b.ymax + 40) * 1j)
+#bases(baseglyphs)
+#bases(belowfullheights)
+#ligs(b6ligs)
+#aboveright(abovedias)
+#abovecentre(centre_marks)
+#belowright(belowdias)
+#belowcentre(belowcentres)
+#clear_aps(allglyphs)
 
-@some
-def belowright(g, a, b, adv):
-    old = a.copy()
-    a.clear()
-    offset = -670j if "low" in g.name else 0
-    addAnchorVal(a, "_L", l_shift + offset)
-    addAnchorVal(a, "_LL", l_shift + 20 + offset)
-    addAnchorVal(a, "LL", l_shift + 20 - 670j + offset)
-    if "def" in old:
-        c = anchorVal(old["def"])
-    else:
-        c = (b.xmin + b.xmax) // 2
-    addAnchorVal(a, "LA", c - 20 - 670j)
-
-@some
-def belowcentre(g, a, b, adv):
-    old = a.copy()
-    a.clear()
-    if "def" in old:
-        c = anchorVal(old["def"])
-    else:
-        c = (b.xmin + b.xmax) // 2
-    addAnchorVal(a, "_LC", c)
-    addAnchorVal(a, "_LA", c - 20)
-    addAnchorVal(a, "LA", c - 20 - 670j)
-    addAnchorVal(a, "LL", l_shift + 20 - 670j)
-
-bases(baseglyphs)
-bases(belowfullheights)
-ligs(b6ligs)
-aboveright(abovedias)
-abovecentre(centre_marks)
-belowright(belowdias)
-belowcentre(belowcentres)
-clear_aps(allglyphs)
+belowlow(belowdias)
 
 if not args.dryrun:
     font.save(args.outfile if args.outfile else args.infile)
 
-print(cover.print_stats())
+# print(cover.print_stats())
